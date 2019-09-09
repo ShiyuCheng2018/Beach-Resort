798fc7376377da35ecaeb35b65e8e8cecebcac0b
Shiyu Cheng <starkeyshiyu@gmail.com>
2019-09-09 15:32:15 -0700

    configured price filter

--- src/Components/RoomFilter.js
+++ src/Components/RoomFilter.js
@@ -23,6 +23,11 @@ export default function RoomFilter({rooms}) {
         return <option value={item} key={index}>{item}</option>
     });
 
+    let people = getUnique(rooms, 'capacity');
+    people = people.map((item, index) => {
+       return <option key={index} value={item}>{item}</option>
+    });
+
     return <section className={"filter-container"}>
         <Title title={"search rooms"}/>
         <form className={"filter-form"}>
@@ -35,6 +40,16 @@ export default function RoomFilter({rooms}) {
 
             </div>
             {/*{ends select type}*/}
+            {/*{guests}*/}
+            <div className={"form-group"}>
+                <label htmlFor={"capacity"}>Guests</label>
+                <select name={"capacity"} id={"capacity"} value={capacity} className={"form-control"} onChange={handleChange}>
+                    {people}
+                </select>
+
+            </div>
+            {/*{ends guests}*/}
+
 
         </form>
 
--- src/context.js
+++ src/context.js
@@ -69,15 +69,23 @@ class RoomProvider extends Component {
         let{
             rooms, type, capacity, price, minSize, maxSize, breakfast, pets
         } = this.state;
-
+        //all the rooms
         let tempRooms = [...rooms];
+        //transform values
+        capacity = parseInt(capacity);
+
+        //filter by type
         if(type !== "all"){
-            tempRooms = tempRooms.filter(room => room.type === type)
+            tempRooms = tempRooms.filter(room => room.type === type);
         }
+        //filter by capacity
+        if(capacity !== 1){
+            tempRooms = tempRooms.filter(room => room.capacity >= capacity);
+        }
+
         this.setState({
             sortedRooms: tempRooms
         })
-
     };
 
     render() {
